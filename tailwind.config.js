dcbcf29702dd5693bb93bb8f8559e28468741b91
Mumraiz Shahzad <mumraiz1996@gmail.com>
2024-10-28 00:03:52 +0500

    changes .json

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,css,html}"],
+  content: ["./index.html", "./src/styles.css/**/*.{js,jsx,ts,tsx,css,html}"],
   theme: {
     extend: {},
   },
   plugins: [],
-};
-module.exports = {
-    theme: {
-        extend: {
-            colors: {
-                gold: {
-                    DEFAULT: '#cda719', // Add the custom gold color
-                },
-            },
-        },
-    },
-};
+}
 
