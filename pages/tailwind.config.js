643d95a6ca947206db55589278b4072359bbfd5a
Mumraiz Shahzad <mumraiz1996@gmail.com>
2024-12-09 11:26:23 +0500

    changes files

--- pages/tailwind.config.js
+++ pages/tailwind.config.js
@@ -1,9 +1,12 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html","./src/styles.css/**/*.{js,jsx,ts,tsx,css,html}"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
+
 
